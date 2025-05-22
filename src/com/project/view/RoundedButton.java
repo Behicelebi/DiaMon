d94f0fdcd24d257443b13787d168b9f98bb89507
Murat Emre Biçici <muratemrebicici@gmail.com>
2025-05-22 18:54:14 +0300

    Press action added!

--- src/com/project/view/RoundedButton.java
+++ src/com/project/view/RoundedButton.java
@@ -26,15 +26,21 @@ public class RoundedButton extends JButton {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(new Color(0, 0, 0, 30));
-        g2.fillRoundRect(2, 4, getWidth() - 4, getHeight() - 4, radius, radius);
-        g2.setColor(Color.WHITE);
+        ButtonModel model = getModel();
+        boolean isPressed = model.isArmed() && model.isPressed();
+
+        if (!isPressed) {
+            g2.setColor(new Color(0, 0, 0, 30));
+            g2.fillRoundRect(2, 4, getWidth() - 4, getHeight() - 4, radius, radius);
+        }
+        g2.setColor(isPressed ? new Color(138, 136, 136) : Color.WHITE);
         g2.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
         g2.setColor(new Color(180, 180, 180));
         g2.setStroke(new BasicStroke(2));
         g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
-        super.paintComponent(g2);
         g2.dispose();
+        super.paintComponent(g);
     }
+
 }
 
